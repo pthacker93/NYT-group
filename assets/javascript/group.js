2989e8932e81411ca24a023a88257bca341acd92
suarezlui <suarezluis@gmail.com>
2018-04-27 11:06:03 -0500

    added push to html

--- assets/javascript/group.js
+++ assets/javascript/group.js
@@ -1,64 +1,48 @@
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 var response = [];
-var term = ""; //prompt("Term");
-var sYear =""; //prompt("Start");
-var eYear = ""; //prompt("eEear");
-var records = 5;
 
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-var response = [];
-var term = "trump"; //prompt("Term");
-var sYear ="20170101"; //prompt("Start");
-var eYear = "20190101"; //prompt("eEear");
-var records = "5";
-
-$("#search").on("click", function(){
-    term = $(".search").val();
-    records = $(".num").val();
-    sYear = $(".start").val();
-    eYear = $(".end").val();
-    
-
-
-
-url +=
-  "?" +
-  $.param({
-    "api-key": "325ff8e5982c49678fea1bbc6766fa05",
-    q: term,
-    begin_date: sYear,
-    end_date: eYear,
-    fl: "headline, web_url, byline"
-  });
-$.ajax({
-  url: url,
-  method: "GET"
-})
-  .done(function(result) {
-   response = (result.response.docs);
-   var card = $("<div>");
-   var head = $("<h3>");
-   var by = $("<p>");
-
-for (let i = 0; i < records; i++) {
-  var current = response[i]
-  console.log(response[i].web_url)
-  console.log(response[i].headline.main)
-  console.log(response[i].byline.original)
-}
-
-
-
-
-
-
-
+$("#search").on("click", function() {
+  term = $(".search").val();
+  records = $(".num").val();
+  sYear = $(".start").val();
+  eYear = $(".end").val();
+  console.log(term);
+  console.log(records);
+  console.log(sYear);
+  console.log(eYear);
+  url +=
+    "?" +
+    $.param({
+      "api-key": "325ff8e5982c49678fea1bbc6766fa05",
+      q: term,
+      begin_date: sYear,
+      end_date: eYear,
+      fl: "headline, web_url, byline"
+    });
+  $.ajax({
+    url: url,
+    method: "GET"
   })
-  .fail(function(err) {
-    throw err;
-  });
+    .done(function(result) {
+      response = result.response.docs;
+      var card = $("<div>");
+      var head = $("<h3>");
+      var by = $("<p>");
 
+      for (let i = 0; i < records; i++) {
+        var current = response[i];
+       
+        
 
-    
-})
-
+        
+        $(".report").append('<a href="'+response[i].web_url+'"><button type="button" class="btn btn-secondary btn-lg btn-block"><p id="articleName"><span id="articleNum">'+ (parseInt(i) + 1) +'</span>' + response[i].headline.main + '</p><p id="articleAuthor"><span id="author">'+ response[i].byline.original +'</span></p></button></a>')
+        
+        console.log(response[i].web_url);
+        console.log(response[i].headline.main);
+        console.log(response[i].byline.original);
+      }
+    })
+    .fail(function(err) {
+      throw err;
+    });
+});
